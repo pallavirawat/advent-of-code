2ddb03cd993a35bc22d59f7e4babe9aaba96f362
Pallavi Rawat <me@rawatpallavi.com>
2020-12-08 22:33:07 +0530

    day 6 | the code is now shorter. small talk because whos got time for anything else.

--- src/main/kotlin/6.kt
+++ src/main/kotlin/6.kt
@@ -1,58 +1,29 @@
 fun main() {
     val input = Reader.read("6.txt") { it }
-    println(findAnswers(input))
-    println(findAnswersToWhichAllAnsweredYes(input))
+
+    val readAndGroup = Reader.readAndGroup("6.txt", groupBy = { it.isEmpty() }) {
+        it.toCharArray()
+    }
+//    6596
+//    3219
+    println(findAnswersWithStyle(readAndGroup))
+    println(findAnswersToWhichAllAnsweredYesWithStyle(readAndGroup))
 }
 
-fun findAnswers(input: List<String>): Int {
-    var count = 0;
-    val groupsAnswers = mutableListOf<Set<Char>>()
-    var groupAnswer = mutableSetOf<Char>();
-    input.forEach {
-        if (it.isBlank()) {
-            groupsAnswers.add(groupAnswer)
-            count+=groupAnswer.size
-            groupAnswer = mutableSetOf<Char>()
-
-        }
-        it.forEach { individualAnswer -> groupAnswer.add(individualAnswer) }
+fun findAnswersToWhichAllAnsweredYesWithStyle(groupedInput: List<List<CharArray>>): Int {
+    return groupedInput.fold(0){acc, elem ->
+        val fold = elem.reduce { acc, chars ->
+            (acc intersect chars.toList()).toCharArray()
+        }.size
+        acc+fold
     }
-    groupsAnswers.add(groupAnswer)
-    count+=groupAnswer.size
-
-    return count
 }
 
-fun findAnswersToWhichAllAnsweredYes(input: List<String>): Int {
-    var count = 0;
-    var groupAnswer = mutableListOf<Char>();
-    var groupSize=0
-    input.forEach {
-        if (it.isBlank()) {
-            val maxYes = groupAnswer.groupBy { charac -> charac }.values.filter {
-                    it -> it.size==groupSize
+fun findAnswersWithStyle(groupedInput: List<List<CharArray>>): Int {
+    return groupedInput.fold(0){acc, elem ->
+            val fold = elem.fold(mutableSetOf()) { acc: Set<Char>, chars ->
+                acc union chars.toSet()
             }
-
-           if (maxYes != null) {
-                count+=maxYes.size
-            }
-            groupSize=0
-            groupAnswer = mutableListOf()
-
-        }
-        else{
-            it.forEach { individualAnswer ->
-                groupAnswer.add(individualAnswer)
-            }
-            groupSize++
-        }
+        acc + fold.size
     }
-    val maxYes = groupAnswer.groupBy { charac -> charac }.values.filter {
-            it -> it.size==groupSize
-    }
-    if (maxYes != null) {
-        count+=maxYes.size
-    }
-
-    return count
 }
--- src/main/kotlin/Reader.kt
+++ src/main/kotlin/Reader.kt
@@ -6,4 +6,20 @@ object Reader {
         }
         return acc
     }
+
+    fun <T>readAndGroup(fileName: String, groupBy: (fileLine: String) -> Boolean , readLineBlock: (fileLine: String) -> T): List<List<T>>{
+        val group = mutableListOf<List<T>>()
+        var acc = mutableListOf<T>()
+        this::class.java.classLoader.getResource(fileName)?.openStream()?.bufferedReader()?.forEachLine {
+            if (groupBy(it)){
+                group.add(acc)
+                acc = mutableListOf()
+            }
+            else{
+                acc.add(readLineBlock(it))
+            }
+        }
+        group.add(acc)
+        return group
+    }
 }
